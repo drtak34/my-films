5417c1aadc0b1026463971829d3b7503576eafd6
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-01-17 22:01:00 +0000

    Updated assembly to 5.0.0.8

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.0.7")]
-[assembly: AssemblyFileVersion("5.0.0.7")]
+[assembly: AssemblyVersion("5.0.0.8")]
+[assembly: AssemblyFileVersion("5.0.0.8")]
 [assembly: NeutralResourcesLanguageAttribute("")]
