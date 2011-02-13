835580007c06237beead45e897c1302acac6786a
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-02-13 16:06:51 +0000

    Updated installer to V5.0.0.20

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.0.19")]
-[assembly: AssemblyFileVersion("5.0.0.19")]
+[assembly: AssemblyVersion("5.0.0.20")]
+[assembly: AssemblyFileVersion("5.0.0.20")]
 [assembly: NeutralResourcesLanguageAttribute("")]
