048cb7abb9a5e40d72870b5be27a5d3ed28765d2
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-04-18 07:20:03 +0000

    Updated installer to V5.0.1.710 added DefaultLogos.B3 to installer

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.1.705")]
-[assembly: AssemblyFileVersion("5.0.1.705")]
+[assembly: AssemblyVersion("5.0.1.710")]
+[assembly: AssemblyFileVersion("5.0.1.710")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 #if MP11
 #else
