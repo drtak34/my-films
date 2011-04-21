56d274f77b08501ffe1d3ab3f9c3ffb120e2f0c2
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-04-21 17:58:36 +0000

    Updated installer to V5.0.1.729 removed unused media files from BG skin

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.1.719")]
-[assembly: AssemblyFileVersion("5.0.1.719")]
+[assembly: AssemblyVersion("5.0.1.729")]
+[assembly: AssemblyFileVersion("5.0.1.729")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 #if MP11
 #else
