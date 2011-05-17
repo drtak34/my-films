3431c0e3eba140ce298b6def55d0754052d19149
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-05-17 02:43:18 +0000

    Fixed installer dependancy updated assembly to V5.0.1.816

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.1.815")]
-[assembly: AssemblyFileVersion("5.0.1.815")]
+[assembly: AssemblyVersion("5.0.1.816")]
+[assembly: AssemblyFileVersion("5.0.1.816")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 #if MP11
 #else
