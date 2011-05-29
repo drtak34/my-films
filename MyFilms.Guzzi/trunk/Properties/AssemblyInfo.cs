67da8d73e2d07295f9751419cfc375bde16ba328
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-05-29 23:39:12 +0000

    Updated assemply and installer to V5.0.1.888

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.1.867")]
-[assembly: AssemblyFileVersion("5.0.1.867")]
+[assembly: AssemblyVersion("5.0.1.888")]
+[assembly: AssemblyFileVersion("5.0.1.888")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 #if MP11
 #else
