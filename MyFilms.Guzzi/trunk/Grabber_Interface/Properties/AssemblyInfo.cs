5844d06977f83fe0e8d56b9910e2a7a6118ebea8
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-06-28 06:15:49 +0000

    Updated installer and assemblies to V5.0.1.1082

--- MyFilms.Guzzi/trunk/Grabber_Interface/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Grabber_Interface/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.1.0.1044")]
-[assembly: AssemblyFileVersion("5.1.0.1044")]
+[assembly: AssemblyVersion("5.1.0.1082")]
+[assembly: AssemblyFileVersion("5.1.0.1082")]
 #if MP12
 //[assembly: CompatibleVersion("1.1.7.0", "1.1.6.0")]
 [assembly: CompatibleVersion("1.1.6.27644")]
