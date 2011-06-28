e93af759a36c1b5bb45186208f60f32cd8ff5495
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-06-28 06:06:12 +0000

    Changed fuzzi matching for exact year hints to "<6"

--- MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using MediaPortal.Common.Utils;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("5.0.1.1044")]
-[assembly: AssemblyFileVersion("5.0.1.1044")]
+[assembly: AssemblyVersion("5.0.1.1079")]
+[assembly: AssemblyFileVersion("5.0.1.1079")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 #if MP11
 #else
