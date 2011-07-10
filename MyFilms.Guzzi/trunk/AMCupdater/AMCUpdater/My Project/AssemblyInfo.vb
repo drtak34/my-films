3adcd80fd6cd4c214347fa79588967c4eb1221ab
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-07-10 08:42:53 +0000

    U0pdated installer and assemblies to V5.0.1.1155

--- MyFilms.Guzzi/trunk/AMCupdater/AMCUpdater/My Project/AssemblyInfo.vb
+++ MyFilms.Guzzi/trunk/AMCupdater/AMCUpdater/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.1.1137")> 
-<Assembly: AssemblyFileVersion("0.9.1.1137")> 
+<Assembly: AssemblyVersion("0.9.1.1155")> 
+<Assembly: AssemblyFileVersion("0.9.1.1155")> 
