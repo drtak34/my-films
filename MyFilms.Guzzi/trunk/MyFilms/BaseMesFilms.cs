a6ed6afbee8165e0c97d791453ff86e4f3cfdbbf
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-07-19 10:07:38 +0000

    Trakt integration: - added "config" to movie object class - added method to sync back watched status via "Commit()" - WIP part 1

--- MyFilms.Guzzi/trunk/MyFilms/BaseMesFilms.cs
+++ MyFilms.Guzzi/trunk/MyFilms/BaseMesFilms.cs
@@ -217,6 +217,7 @@ namespace MyFilmsPlugin.MyFilms
             foreach (string config in configs)
             {
               string Catalog = XmlConfig.ReadXmlConfig("MyFilms", config, "AntCatalog", string.Empty);
+              // string StrFileType = XmlConfig.ReadXmlConfig("MyFilms", config, "CatalogType", "0");
               bool TraktEnabled = XmlConfig.ReadXmlConfig("MyFilms", config, "AllowTraktSync", false);
               string StrTitle1 = XmlConfig.ReadXmlConfig("MyFilms", config, "AntTitle1", string.Empty);
               string StrDfltSelect = XmlConfig.ReadXmlConfig("MyFilms", config, "StrDfltSelect", string.Empty);
@@ -249,6 +250,7 @@ namespace MyFilmsPlugin.MyFilms
                     try
                     {
                       MFMovie movie = new MFMovie();
+                      movie.Config = config; // MF config context
                       if (!string.IsNullOrEmpty(sr["Number"].ToString()))
                         movie.ID = Int32.Parse(sr["Number"].ToString());
                       else movie.ID = 0;
@@ -319,119 +321,170 @@ namespace MyFilmsPlugin.MyFilms
 
         public static string Translate_Column(string Column)
         {
-            string s = Column;
-	          // Check for empty string.
-	          if (string.IsNullOrEmpty(s))
-	          {
-	              return string.Empty;
-	          }
-	          // Return char and concat substring.
-	          Column = char.ToUpper(s[0]) + s.Substring(1);
+            //string s = Column;
+            //// Check for empty string.
+            //if (string.IsNullOrEmpty(s))
+            //{
+            //    return string.Empty;
+            //}
+            //// Return char and concat substring.
+            //Column = char.ToUpper(s[0]) + s.Substring(1);
 
             switch (Column)
             {
-                case "Number":
-                    return GUILocalizeStrings.Get(10798650);
-                case "Checked":
-                    return GUILocalizeStrings.Get(10798651);
-                case "MediaLabel":
-                    return GUILocalizeStrings.Get(10798652);
-                case "MediaType":
-                    return GUILocalizeStrings.Get(10798653);
-                case "Source":
-                    return GUILocalizeStrings.Get(10798654);
-                case "Date":
-                    return GUILocalizeStrings.Get(10798655);
-                case "Borrower":
-                    return GUILocalizeStrings.Get(10798656);
-                case "Rating":
-                    return GUILocalizeStrings.Get(10798657);
-                case "OriginalTitle":
-                    return GUILocalizeStrings.Get(10798658);
-                case "TranslatedTitle":
-                    return GUILocalizeStrings.Get(10798659);
-                case "FormattedTitle":
-                    return GUILocalizeStrings.Get(10798660);
-                case "Director":
-                    return GUILocalizeStrings.Get(10798661);
-                case "Producer":
-                    return GUILocalizeStrings.Get(10798662);
-                case "Country":
-                    return GUILocalizeStrings.Get(10798663);
-                case "Category":
-                    return GUILocalizeStrings.Get(10798664);
-                case "Year":
-                    return GUILocalizeStrings.Get(10798665);
-                case "Length":
-                    return GUILocalizeStrings.Get(10798666);
-                case "Actors":
-                    return GUILocalizeStrings.Get(10798667);
-                case "URL":
-                    return GUILocalizeStrings.Get(10798668);
-                case "Description":
-                    return GUILocalizeStrings.Get(10798669);
-                case "Comments":
-                    return GUILocalizeStrings.Get(10798670);
-                case "VideoFormat":
-                    return GUILocalizeStrings.Get(10798671);
-                case "VideoBitrate":
-                    return GUILocalizeStrings.Get(10798672);
-                case "AudioFormat":
-                    return GUILocalizeStrings.Get(10798673);
-                case "AudioBitrate":
-                    return GUILocalizeStrings.Get(10798674);
-                case "Resolution":
-                    return GUILocalizeStrings.Get(10798675);
-                case "Framerate":
-                    return GUILocalizeStrings.Get(10798676);
-                case "Languages":
-                    return GUILocalizeStrings.Get(10798677);
-                case "Subtitles":
-                    return GUILocalizeStrings.Get(10798678);
-                case "DateAdded":
-                    return GUILocalizeStrings.Get(10798679);
-                case "Size":
-                    return GUILocalizeStrings.Get(10798680);
-                case "Disks":
-                    return GUILocalizeStrings.Get(10798681);
-                case "Picture":
-                    return GUILocalizeStrings.Get(10798682);
-                case "Certification":
-                    return GUILocalizeStrings.Get(10798683);
-                case "Writer":
-                    return GUILocalizeStrings.Get(10798684);
-                case "Watched":
-                    return GUILocalizeStrings.Get(10798685);
-                case "WatchedDate":
-                    return GUILocalizeStrings.Get(10798686);
-                case "IMDB_Id":
-                    return GUILocalizeStrings.Get(10798687);
-                case "TMDB_Id":
-                    return GUILocalizeStrings.Get(10798688);
-                case "SourceTrailer":
-                    return GUILocalizeStrings.Get(10798940);
-                case "TagLine":
-                    return GUILocalizeStrings.Get(10798941);
-                case "Tags":
-                    return GUILocalizeStrings.Get(10798942);
-                case "Aspectratio":
-                    return GUILocalizeStrings.Get(10798943);
-                case "RatingUser":
-                    return GUILocalizeStrings.Get(10798944);
-                case "Fanart":
-                    return GUILocalizeStrings.Get(10798945);
-                case "Studio":
-                    return GUILocalizeStrings.Get(10798946);
-                case "IMDB_Rank":
-                    return GUILocalizeStrings.Get(10798947);
-                case "IsOnline":
-                    return GUILocalizeStrings.Get(10798948);
-                case "Edition":
-                    return GUILocalizeStrings.Get(10798949);
-                case "IsOnlineTrailer":
-                    return GUILocalizeStrings.Get(10798950);
-                default:
-                    return string.Empty;
+              case "Number":
+              case "number":
+                return GUILocalizeStrings.Get(10798650);
+              case "Checked":
+              case "checked":
+                return GUILocalizeStrings.Get(10798651);
+              case "MediaLabel":
+              case "medialabel":
+                return GUILocalizeStrings.Get(10798652);
+              case "MediaType":
+              case "mediatype":
+                return GUILocalizeStrings.Get(10798653);
+              case "Source":
+              case "source":
+                return GUILocalizeStrings.Get(10798654);
+              case "Date":
+              case "date":
+                return GUILocalizeStrings.Get(10798655);
+              case "Borrower":
+              case "borrower":
+                return GUILocalizeStrings.Get(10798656);
+              case "Rating":
+              case "rating":
+                return GUILocalizeStrings.Get(10798657);
+              case "OriginalTitle":
+              case "originaltitle":
+                return GUILocalizeStrings.Get(10798658);
+              case "TranslatedTitle":
+              case "translatedtitle":
+                return GUILocalizeStrings.Get(10798659);
+              case "FormattedTitle":
+              case "formattedtitle":
+                return GUILocalizeStrings.Get(10798660);
+              case "Director":
+              case "director":
+                return GUILocalizeStrings.Get(10798661);
+              case "Producer":
+              case "producer":
+                return GUILocalizeStrings.Get(10798662);
+              case "Country":
+              case "country":
+                return GUILocalizeStrings.Get(10798663);
+              case "Category":
+              case "category":
+                return GUILocalizeStrings.Get(10798664);
+              case "Year":
+              case "year":
+                return GUILocalizeStrings.Get(10798665);
+              case "Length":
+              case "length":
+                return GUILocalizeStrings.Get(10798666);
+              case "Actors":
+              case "actors":
+                return GUILocalizeStrings.Get(10798667);
+              case "URL":
+              case "url":
+                return GUILocalizeStrings.Get(10798668);
+              case "Description":
+              case "description":
+                return GUILocalizeStrings.Get(10798669);
+              case "Comments":
+              case "comments":
+                return GUILocalizeStrings.Get(10798670);
+              case "VideoFormat":
+              case "videoformat":
+                return GUILocalizeStrings.Get(10798671);
+              case "VideoBitrate":
+              case "videobitrate":
+                return GUILocalizeStrings.Get(10798672);
+              case "AudioFormat":
+              case "audioformat":
+                return GUILocalizeStrings.Get(10798673);
+              case "AudioBitrate":
+              case "audiobitrate":
+                return GUILocalizeStrings.Get(10798674);
+              case "Resolution":
+              case "resolution":
+                return GUILocalizeStrings.Get(10798675);
+              case "Framerate":
+              case "framerate":
+                return GUILocalizeStrings.Get(10798676);
+              case "Languages":
+              case "languages":
+                return GUILocalizeStrings.Get(10798677);
+              case "Subtitles":
+              case "subtitles":
+                return GUILocalizeStrings.Get(10798678);
+              case "DateAdded":
+              case "dateadded":
+                return GUILocalizeStrings.Get(10798679);
+              case "Size":
+              case "size":
+                return GUILocalizeStrings.Get(10798680);
+              case "Disks":
+              case "disks":
+                return GUILocalizeStrings.Get(10798681);
+              case "Picture":
+              case "picture":
+                return GUILocalizeStrings.Get(10798682);
+              case "Certification":
+              case "certification":
+                return GUILocalizeStrings.Get(10798683);
+              case "Writer":
+              case "writer":
+                return GUILocalizeStrings.Get(10798684);
+              case "Watched":
+              case "watched":
+                return GUILocalizeStrings.Get(10798685);
+              case "WatchedDate":
+              case "watcheddate":
+                return GUILocalizeStrings.Get(10798686);
+              case "IMDB_Id":
+              case "imdb_id":
+                return GUILocalizeStrings.Get(10798687);
+              case "TMDB_Id":
+              case "tmdb_id":
+                return GUILocalizeStrings.Get(10798688);
+              case "SourceTrailer":
+              case "sourcetrailer":
+                return GUILocalizeStrings.Get(10798940);
+              case "TagLine":
+              case "tagline":
+                return GUILocalizeStrings.Get(10798941);
+              case "Tags":
+              case "tags":
+                return GUILocalizeStrings.Get(10798942);
+              case "Aspectratio":
+              case "aspectratio":
+                return GUILocalizeStrings.Get(10798943);
+              case "RatingUser":
+              case "ratinguser":
+                return GUILocalizeStrings.Get(10798944);
+              case "Fanart":
+              case "fanart":
+                return GUILocalizeStrings.Get(10798945);
+              case "Studio":
+              case "studio":
+                return GUILocalizeStrings.Get(10798946);
+              case "IMDB_Rank":
+              case "imdb_rank":
+                return GUILocalizeStrings.Get(10798947);
+              case "IsOnline":
+              case "isonline":
+                return GUILocalizeStrings.Get(10798948);
+              case "Edition":
+              case "edition":
+                return GUILocalizeStrings.Get(10798949);
+              case "IsOnlineTrailer":
+              case "isonlinetrailer":
+                return GUILocalizeStrings.Get(10798950);
+
+              default:
+                return string.Empty;
             }
         }
         #endregion
@@ -450,9 +503,10 @@ namespace MyFilmsPlugin.MyFilms
     private string _mDateAdded = string.Empty;
     private string _mPicture = string.Empty;
     private string _mFanart = string.Empty;
+    private string _mConfig = string.Empty;
 
     public MFMovie() { }
-
+    
     public int ID
     {
       get { return _mID; }
@@ -537,6 +591,12 @@ namespace MyFilmsPlugin.MyFilms
       set { _mFanart = value; }
     }
 
+    public string Config
+    {
+      get { return _mConfig; }
+      set { _mConfig = value; }
+    }
+
     public void Reset()
     {
       _mStrTitle = string.Empty;
@@ -548,6 +608,104 @@ namespace MyFilmsPlugin.MyFilms
       _mDateAdded = string.Empty;
       _mPicture = string.Empty;
       _mFanart = string.Empty;
+      _mConfig = string.Empty;
+    }
+
+    public void Commit()
+    {
+      //_mStrTitle = string.Empty;
+      //_mStrIMDBNumber = string.Empty;
+      //_mStrTMDBNumber = string.Empty;
+      //_mIYear = 1900;
+      //_mFRating = 0.0f;
+      //_mIWatched = false;
+      //_mDateAdded = string.Empty;
+      //_mPicture = string.Empty;
+      //_mFanart = string.Empty;
+      //_mConfig = string.Empty;
+
+      AntMovieCatalog dataImport = new AntMovieCatalog();
+
+      using (XmlSettings XmlConfig = new XmlSettings(MediaPortal.Configuration.Config.GetFile(MediaPortal.Configuration.Config.Dir.Config, "MyFilms.xml")))
+      {
+          string config = _mConfig;
+          //Configuration confimport = new Configuration(config, true);
+          //Configuration.Current_Config();
+          //MyFilms.Load_Config(Configuration.config, true);
+          string Catalog = XmlConfig.ReadXmlConfig("MyFilms", config, "AntCatalog", string.Empty);
+          string StrFileType = XmlConfig.ReadXmlConfig("MyFilms", config, "CatalogType", "0");
+          bool TraktEnabled = XmlConfig.ReadXmlConfig("MyFilms", config, "AllowTraktSync", false);
+          string StrTitle1 = XmlConfig.ReadXmlConfig("MyFilms", config, "AntTitle1", string.Empty);
+          string StrDfltSelect = XmlConfig.ReadXmlConfig("MyFilms", config, "StrDfltSelect", string.Empty);
+          bool EnhancedWatchedStatusHandling = XmlConfig.ReadXmlConfig("MyFilms", config, "EnhancedWatchedStatusHandling", false);
+          string GlobalUnwatchedOnlyValue = XmlConfig.ReadXmlConfig("MyFilms", config, "GlobalUnwatchedOnlyValue", "false");
+          string WatchedField = XmlConfig.ReadXmlConfig("MyFilms", config, "WatchedField", "Checked");
+          string UserProfileName = XmlConfig.ReadXmlConfig("MyFilms", config, "UserProfileName", "");
+
+
+          // LogMyFilms.Debug("GetMovies: TraktSync = '" + TraktEnabled + "', Config = '" + config + "', Catalogfile = '" + Catalog + "'");
+          if (System.IO.File.Exists(Catalog) && TraktEnabled)
+          {
+            try
+            {
+              dataImport.ReadXml(Catalog);
+            }
+            catch (Exception e)
+            {
+              // LogMyFilms.Error(": Error reading xml database after " + data.Movie.Count.ToString() + " records; error : " + e.Message.ToString() + ", " + e.StackTrace.ToString());
+              // throw e;
+            }
+
+            try
+            {
+              DataRow[] results = dataImport.Tables["Movie"].Select(StrDfltSelect + "Number" + " like " + _mID, "OriginalTitle" + " " + "ASC");
+              // if (results.Length == 0) continue;
+              //MyFilms._rw.EnterReadLock();
+              //r = BaseMesFilms.ReadDataMovies(GlobalFilterString + conf.StrDfltSelect, conf.StrFilmSelect, conf.StrSorta, conf.StrSortSens, false);
+              //MyFilms._rw.ExitReadLock();
+              //MyFilmsDetail.Suppress_Entry((DataRow[])MyFilms.r, (int)facadeView.SelectedListItem.ItemId);
+
+              foreach (DataRow sr in results)
+              {
+                try
+                {
+                  if (!EnhancedWatchedStatusHandling)
+                  {
+                    if (_mIWatched)
+                      // MyFilmsDetail.Watched_Toggle(Index, true);
+                      sr[WatchedField] = "true";
+                    else
+                      // MyFilmsDetail.Watched_Toggle(Index, true);
+                      sr[WatchedField] = GlobalUnwatchedOnlyValue;
+                  }
+                  if (!string.IsNullOrEmpty(_mStrIMDBNumber))
+                    sr["IMDB_Id"] = _mStrIMDBNumber;
+                }
+                catch (Exception mex)
+                {
+                  Log.Error("MyFilms videodatabase: add movie exception - err:{0} stack:{1}", mex.Message, mex.StackTrace);
+                  throw;
+                }
+              }
+            }
+            catch (Exception ex)
+            {
+              // LogMyFilms.Error("MyFilms videodatabase exception err:{0} stack:{1}", ex.Message, ex.StackTrace);
+              Log.Error("MyFilms videodatabase exception err:{0} stack:{1}", ex.Message, ex.StackTrace);
+            }
+            try
+            {
+              System.Xml.XmlTextWriter MyXmlTextWriter = new System.Xml.XmlTextWriter(Catalog, System.Text.Encoding.Default);
+              MyXmlTextWriter.Formatting = System.Xml.Formatting.Indented; // Added by Guzzi to get properly formatted output XML
+              MyXmlTextWriter.WriteStartDocument();
+              dataImport.WriteXml(MyXmlTextWriter, XmlWriteMode.IgnoreSchema);
+              MyXmlTextWriter.Close();
+            }
+            catch (Exception) { }
+          }
+        } 
+    
+    
     }
   }
 }
