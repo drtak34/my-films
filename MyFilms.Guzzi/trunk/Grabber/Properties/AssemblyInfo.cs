1757a69083d5de1b7e963340b15ca4f23f00f250
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-08-13 14:37:41 +0000

    Updated Assemblies and installer to V5.0.2.1302 for MP1.2-RC

--- MyFilms.Guzzi/trunk/Grabber/Properties/AssemblyInfo.cs
+++ MyFilms.Guzzi/trunk/Grabber/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.0.1.1286")]
-[assembly: AssemblyFileVersion("5.0.1.1286")]
+[assembly: AssemblyVersion("5.0.2.1302")]
+[assembly: AssemblyFileVersion("5.0.2.1302")]
 #if MP12
 //[assembly: CompatibleVersion("1.1.7.0", "1.1.6.0")]
 [assembly: CompatibleVersion("1.1.6.27644")]
