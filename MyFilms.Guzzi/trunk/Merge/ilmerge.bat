86937119962844069e48b30642cfb64346cc4f45
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-09-01 23:34:28 +0000

    Solution Cleanup

--- MyFilms.Guzzi/trunk/Merge/ilmerge.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-IF EXIST MyFilms_UNMERGED.dll del MyFilms_UNMERGED.dll
-ren MyFilms.dll MyFilms_UNMERGED.dll
-ilmerge /out:MyFilms.dll MyFilms_UNMERGED.dll NLog.dll Grabber.dll TaskScheduler.dll
-rem ilmerge /out:MesFilms.dll MesFilms_UNMERGED.dll Cornerstone.MP.dll NLog.dll AMCupdater.exe TaskScheduler.dll
