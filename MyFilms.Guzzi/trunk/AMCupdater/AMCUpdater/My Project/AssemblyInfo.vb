0bba5144b0ae4b3db0338a92b34659544b064d49
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-09-02 00:51:00 +0000

    Updated AMCupdater project to autoset build number to assembly version

--- MyFilms.Guzzi/trunk/AMCupdater/AMCUpdater/My Project/AssemblyInfo.vb
+++ MyFilms.Guzzi/trunk/AMCupdater/AMCUpdater/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.0.2.1372")> 
-<Assembly: AssemblyFileVersion("5.0.2.1372")> 
+'<Assembly: AssemblyVersion("5.0.2.1372")> 
+'<Assembly: AssemblyFileVersion("5.0.2.1372")> 
