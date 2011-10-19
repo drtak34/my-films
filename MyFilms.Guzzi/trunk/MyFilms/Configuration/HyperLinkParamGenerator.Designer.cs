ac923277f83c42a6fb5727e958bc6c2dda4acc01
Iggo Gunzelmann <Iggo Gunzelmann@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-10-19 18:31:59 +0000

    Rework Startparameter Creator as asked by Dadeo

--- MyFilms.Guzzi/trunk/MyFilms/Configuration/HyperLinkParamGenerator.Designer.cs
+++ MyFilms.Guzzi/trunk/MyFilms/Configuration/HyperLinkParamGenerator.Designer.cs
@@ -31,6 +31,9 @@
       this.components = new System.ComponentModel.Container();
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HyperLinkParamGenerator));
       this.groupBox3 = new System.Windows.Forms.GroupBox();
+      this.btnCancel = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
       this.tbEditorStartParamsOutput = new System.Windows.Forms.TextBox();
       this.btnSaveEditorStartParams = new System.Windows.Forms.Button();
       this.label60 = new System.Windows.Forms.Label();
@@ -44,15 +47,24 @@
       this.cbEditorViews = new System.Windows.Forms.ComboBox();
       this.cbEditorConfigs = new System.Windows.Forms.ComboBox();
       this.cbEditorViewValues = new System.Windows.Forms.ComboBox();
-      this.label1 = new System.Windows.Forms.Label();
       this.toolTipHyperlinkStartParams = new System.Windows.Forms.ToolTip(this.components);
-      this.label2 = new System.Windows.Forms.Label();
-      this.btnCancel = new System.Windows.Forms.Button();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.label7 = new System.Windows.Forms.Label();
+      this.label8 = new System.Windows.Forms.Label();
       this.groupBox3.SuspendLayout();
       this.SuspendLayout();
       // 
       // groupBox3
       // 
+      this.groupBox3.Controls.Add(this.label8);
+      this.groupBox3.Controls.Add(this.label7);
+      this.groupBox3.Controls.Add(this.label6);
+      this.groupBox3.Controls.Add(this.label5);
+      this.groupBox3.Controls.Add(this.label4);
+      this.groupBox3.Controls.Add(this.label3);
       this.groupBox3.Controls.Add(this.btnCancel);
       this.groupBox3.Controls.Add(this.label2);
       this.groupBox3.Controls.Add(this.label1);
@@ -71,22 +83,50 @@
       this.groupBox3.Controls.Add(this.cbEditorViewValues);
       this.groupBox3.Location = new System.Drawing.Point(12, 12);
       this.groupBox3.Name = "groupBox3";
-      this.groupBox3.Size = new System.Drawing.Size(659, 186);
+      this.groupBox3.Size = new System.Drawing.Size(678, 270);
       this.groupBox3.TabIndex = 94;
       this.groupBox3.TabStop = false;
       this.groupBox3.Text = "Start Parameter Creator ...";
       this.toolTipHyperlinkStartParams.SetToolTip(this.groupBox3, resources.GetString("groupBox3.ToolTip"));
       // 
+      // btnCancel
+      // 
+      this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.btnCancel.Location = new System.Drawing.Point(232, 135);
+      this.btnCancel.Name = "btnCancel";
+      this.btnCancel.Size = new System.Drawing.Size(75, 23);
+      this.btnCancel.TabIndex = 107;
+      this.btnCancel.Text = "Cancel";
+      this.btnCancel.UseVisualStyleBackColor = true;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(44, 230);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(376, 13);
+      this.label2.TabIndex = 106;
+      this.label2.Text = "Paste the \"Resulting Start Parameters\" as hyperlink on any button in your skin.";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(44, 175);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(132, 13);
+      this.label1.TabIndex = 105;
+      this.label1.Text = "Resulting Start Parameters";
+      // 
       // tbEditorStartParamsOutput
       // 
-      this.tbEditorStartParamsOutput.Location = new System.Drawing.Point(15, 112);
+      this.tbEditorStartParamsOutput.Location = new System.Drawing.Point(43, 191);
       this.tbEditorStartParamsOutput.Name = "tbEditorStartParamsOutput";
-      this.tbEditorStartParamsOutput.Size = new System.Drawing.Size(625, 20);
+      this.tbEditorStartParamsOutput.Size = new System.Drawing.Size(613, 20);
       this.tbEditorStartParamsOutput.TabIndex = 104;
       // 
       // btnSaveEditorStartParams
       // 
-      this.btnSaveEditorStartParams.Location = new System.Drawing.Point(501, 147);
+      this.btnSaveEditorStartParams.Location = new System.Drawing.Point(47, 135);
       this.btnSaveEditorStartParams.Name = "btnSaveEditorStartParams";
       this.btnSaveEditorStartParams.Size = new System.Drawing.Size(139, 23);
       this.btnSaveEditorStartParams.TabIndex = 103;
@@ -97,7 +137,7 @@
       // label60
       // 
       this.label60.AutoSize = true;
-      this.label60.Location = new System.Drawing.Point(420, 16);
+      this.label60.Location = new System.Drawing.Point(458, 66);
       this.label60.Name = "label60";
       this.label60.Size = new System.Drawing.Size(95, 13);
       this.label60.TabIndex = 102;
@@ -105,7 +145,7 @@
       // 
       // tbEditorSearchExpression
       // 
-      this.tbEditorSearchExpression.Location = new System.Drawing.Point(418, 32);
+      this.tbEditorSearchExpression.Location = new System.Drawing.Point(456, 82);
       this.tbEditorSearchExpression.Name = "tbEditorSearchExpression";
       this.tbEditorSearchExpression.Size = new System.Drawing.Size(100, 20);
       this.tbEditorSearchExpression.TabIndex = 101;
@@ -113,7 +153,7 @@
       // label59
       // 
       this.label59.AutoSize = true;
-      this.label59.Location = new System.Drawing.Point(540, 16);
+      this.label59.Location = new System.Drawing.Point(574, 66);
       this.label59.Name = "label59";
       this.label59.Size = new System.Drawing.Size(39, 13);
       this.label59.TabIndex = 100;
@@ -129,16 +169,16 @@
             "Small Thumbs",
             "Filmstrip",
             "Coverflow"});
-      this.cbEditorLayout.Location = new System.Drawing.Point(538, 32);
+      this.cbEditorLayout.Location = new System.Drawing.Point(572, 82);
       this.cbEditorLayout.Name = "cbEditorLayout";
-      this.cbEditorLayout.Size = new System.Drawing.Size(102, 21);
+      this.cbEditorLayout.Size = new System.Drawing.Size(84, 21);
       this.cbEditorLayout.TabIndex = 99;
       this.cbEditorLayout.SelectedIndexChanged += new System.EventHandler(this.cbEditorLayout_SelectedIndexChanged);
       // 
       // label58
       // 
       this.label58.AutoSize = true;
-      this.label58.Location = new System.Drawing.Point(151, 16);
+      this.label58.Location = new System.Drawing.Point(54, 66);
       this.label58.Name = "label58";
       this.label58.Size = new System.Drawing.Size(82, 13);
       this.label58.TabIndex = 98;
@@ -147,7 +187,7 @@
       // label57
       // 
       this.label57.AutoSize = true;
-      this.label57.Location = new System.Drawing.Point(286, 16);
+      this.label57.Location = new System.Drawing.Point(188, 66);
       this.label57.Name = "label57";
       this.label57.Size = new System.Drawing.Size(77, 13);
       this.label57.TabIndex = 97;
@@ -156,7 +196,7 @@
       // label52
       // 
       this.label52.AutoSize = true;
-      this.label52.Location = new System.Drawing.Point(286, 56);
+      this.label52.Location = new System.Drawing.Point(324, 66);
       this.label52.Name = "label52";
       this.label52.Size = new System.Drawing.Size(104, 13);
       this.label52.TabIndex = 96;
@@ -164,7 +204,7 @@
       // 
       // btnLoadEditorValues
       // 
-      this.btnLoadEditorValues.Location = new System.Drawing.Point(15, 30);
+      this.btnLoadEditorValues.Location = new System.Drawing.Point(47, 25);
       this.btnLoadEditorValues.Name = "btnLoadEditorValues";
       this.btnLoadEditorValues.Size = new System.Drawing.Size(121, 23);
       this.btnLoadEditorValues.TabIndex = 93;
@@ -175,7 +215,7 @@
       // cbEditorViews
       // 
       this.cbEditorViews.FormattingEnabled = true;
-      this.cbEditorViews.Location = new System.Drawing.Point(284, 32);
+      this.cbEditorViews.Location = new System.Drawing.Point(186, 82);
       this.cbEditorViews.Name = "cbEditorViews";
       this.cbEditorViews.Size = new System.Drawing.Size(121, 21);
       this.cbEditorViews.TabIndex = 95;
@@ -184,7 +224,7 @@
       // cbEditorConfigs
       // 
       this.cbEditorConfigs.FormattingEnabled = true;
-      this.cbEditorConfigs.Location = new System.Drawing.Point(149, 32);
+      this.cbEditorConfigs.Location = new System.Drawing.Point(47, 82);
       this.cbEditorConfigs.Name = "cbEditorConfigs";
       this.cbEditorConfigs.Size = new System.Drawing.Size(121, 21);
       this.cbEditorConfigs.TabIndex = 94;
@@ -193,19 +233,10 @@
       // cbEditorViewValues
       // 
       this.cbEditorViewValues.FormattingEnabled = true;
-      this.cbEditorViewValues.Location = new System.Drawing.Point(284, 72);
+      this.cbEditorViewValues.Location = new System.Drawing.Point(322, 82);
       this.cbEditorViewValues.Name = "cbEditorViewValues";
       this.cbEditorViewValues.Size = new System.Drawing.Size(121, 21);
       this.cbEditorViewValues.TabIndex = 6;
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(16, 96);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(132, 13);
-      this.label1.TabIndex = 105;
-      this.label1.Text = "Resulting Start Parameters";
       // 
       // toolTipHyperlinkStartParams
       // 
@@ -213,32 +244,67 @@
       this.toolTipHyperlinkStartParams.InitialDelay = 500;
       this.toolTipHyperlinkStartParams.ReshowDelay = 100;
       // 
-      // label2
+      // label3
       // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(16, 144);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(376, 26);
-      this.label2.TabIndex = 106;
-      this.label2.Text = "\"Create StartParameters\" will create those and copy them to your clipboard.\r\nPast" +
-          "e the \"Resulting Start Parameters\" as hyperlink on any button in your skin.";
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 30);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(16, 13);
+      this.label3.TabIndex = 108;
+      this.label3.Text = "1.";
       // 
-      // btnCancel
+      // label4
       // 
-      this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.btnCancel.Location = new System.Drawing.Point(406, 147);
-      this.btnCancel.Name = "btnCancel";
-      this.btnCancel.Size = new System.Drawing.Size(75, 23);
-      this.btnCancel.TabIndex = 107;
-      this.btnCancel.Text = "Cancel";
-      this.btnCancel.UseVisualStyleBackColor = true;
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(12, 85);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(16, 13);
+      this.label4.TabIndex = 109;
+      this.label4.Text = "2.";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(12, 140);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(16, 13);
+      this.label5.TabIndex = 110;
+      this.label5.Text = "3.";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(327, 133);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(306, 26);
+      this.label6.TabIndex = 111;
+      this.label6.Text = "\"Create StartParameters\" will create thos and copy them below \r\n and to your clip" +
+          "board";
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(188, 30);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(216, 13);
+      this.label7.TabIndex = 112;
+      this.label7.Text = "THen select the parameters you wish below.";
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(12, 230);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(16, 13);
+      this.label8.TabIndex = 113;
+      this.label8.Text = "4.";
       // 
       // HyperLinkParamGenerator
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this.btnCancel;
-      this.ClientSize = new System.Drawing.Size(683, 210);
+      this.ClientSize = new System.Drawing.Size(702, 294);
       this.Controls.Add(this.groupBox3);
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "HyperLinkParamGenerator";
@@ -269,5 +335,11 @@
     private System.Windows.Forms.ToolTip toolTipHyperlinkStartParams;
     private System.Windows.Forms.Button btnCancel;
     private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label8;
   }
 }
