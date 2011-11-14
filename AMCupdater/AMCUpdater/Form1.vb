206a3e5ecf5bc4b2edd3c676a2f1b1f5e6dc441a
Guzzi <Guzzi@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2011-11-14 00:27:18 +0000

    (Fixes issue #51) : Exception error in Movie Catalog tab (AMCupdater) -> removed Person Tab and Movie Catalog Tab from Release build, as it's only for development

--- AMCupdater/AMCUpdater/Form1.vb
+++ AMCupdater/AMCUpdater/Form1.vb
@@ -35,17 +35,61 @@ Public Class Form1
 #If Config = "Release" Then
         ToolStripMenuItemDebug.Visible = False
         ToolStripMenuItemOptions.Visible = False
-        ViewPersons.Visible = False
-        ViewCatalog.Visible = False
+        HideTabPage(ViewPersons)
+        HideTabPage(ViewCatalog)
 #Else
         ToolStripMenuItemDebug.Visible = True
         ToolStripMenuItemOptions.Visible = True
-        ViewPersons.Visible = True
-        ViewCatalog.Visible = True
 #End If
 
 
     End Sub
+    Private Sub HideTabPage(ByVal tp As TabPage)
+        If TabControl1.TabPages.Contains(tp) Then TabControl1.TabPages.Remove(tp)
+    End Sub
+
+    Private Sub ShowTabPage(ByVal tp As TabPage)
+        ShowTabPage(tp, TabControl1.TabPages.Count)
+    End Sub
+
+    Private Sub ShowTabPage(ByVal tp As TabPage, ByVal index As Integer)
+        If TabControl1.TabPages.Contains(tp) Then Return
+        InsertTabPage(tp, index)
+    End Sub
+
+    Private Sub InsertTabPage(ByVal [tabpage] As TabPage, ByVal [index] As Integer)
+        If [index] < 0 Or [index] > TabControl1.TabCount Then
+            Throw New ArgumentException("Index out of Range.")
+        End If
+        TabControl1.TabPages.Add([tabpage])
+        If [index] < TabControl1.TabCount - 1 Then
+            Do While TabControl1.TabPages.IndexOf([tabpage]) <> [index]
+                SwapTabPages([tabpage], (TabControl1.TabPages(TabControl1.TabPages.IndexOf([tabpage]) - 1)))
+            Loop
+        End If
+        TabControl1.SelectedTab = [tabpage]
+    End Sub
+
+    Private Sub SwapTabPages(ByVal tp1 As TabPage, ByVal tp2 As TabPage)
+        If TabControl1.TabPages.Contains(tp1) = False Or TabControl1.TabPages.Contains(tp2) = False Then
+            Throw New ArgumentException("TabPages must be in the TabCotrols TabPageCollection.")
+        End If
+        Dim Index1 As Integer = TabControl1.TabPages.IndexOf(tp1)
+        Dim Index2 As Integer = TabControl1.TabPages.IndexOf(tp2)
+        TabControl1.TabPages(Index1) = tp2
+        TabControl1.TabPages(Index2) = tp1
+
+        'Uncomment the following section to overcome bugs in the Compact Framework
+        'TabControl1.SelectedIndex = TabControl1.SelectedIndex 
+        'Dim tp1Text, tp2Text As String
+        'tp1Text = tp1.Text
+        'tp2Text = tp2.Text
+        'tp1.Text=tp2Text
+        'tp2.Text=tp1Text
+
+    End Sub
+
+
 
     Private Sub Form1_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Enter
 
