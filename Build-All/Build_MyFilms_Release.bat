0620973212e9e7d0514984712ff7d256d8357eba
Mikeplanet5 <Mikeplanet5@web.de>
2011-12-17 09:20:09 +0000

    Moved command line build scripts into build project dir

--- Build-All/Build_MyFilms_Release.bat
+++ Build-All/Build_MyFilms_Release.bat
@@ -31,9 +31,9 @@ echo Building MyFilms...
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=%BUILD_TYPE% "..\MyFilmsSuite.sln" >> %LOG%
 
 rem echo Building MPEI
-rem copy "..\MPEI\MyFilms_MP12.xmp2" "..\MPEI\MyFilms_MP12_COPY.xmp2"
-rem "%progpath%\Team MediaPortal\MediaPortal\MpeMaker.exe" "..\MPEI\MyFilms_MP12_COPY.xmp2" /B >> %LOG%
-rem del "..\MPEI\MyFilms_MP12_COPY.xmp2"
+rem copy "..\Installer\MyFilms.xmp2" "..\Installer\MyFilms_COPY.xmp2"
+rem "%progpath%\Team MediaPortal\MediaPortal\MpeMaker.exe" "..\Installer\MyFilms_COPY.xmp2" /B >> %LOG%
+rem del "..\Installer\MyFilms_COPY.xmp2"
 
 echo.
 echo Building MyFilms done.
