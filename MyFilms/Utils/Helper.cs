53d71e3680d77ec452b228778914e765c1e159be
Mikeplanet5 <Mikeplanet5@web.de>
2012-08-20 23:27:24 +0000

    (Fixes issue #175) : In environments with central databases, Trakt upates take a long time and lock GUI actions

--- MyFilms/Utils/Helper.cs
+++ MyFilms/Utils/Helper.cs
@@ -25,6 +25,7 @@ namespace MyFilmsPlugin.MyFilms.Utils
 {
   using System;
   using System.Collections;
+  using System.Data;
   using System.IO;
   using System.Collections.Generic;
   using System.Linq;
@@ -32,6 +33,7 @@ namespace MyFilmsPlugin.MyFilms.Utils
   using System.Diagnostics;
   using System.Runtime.InteropServices;
   using System.Text;
+  using System.Threading;
 
   using MediaPortal.Configuration;
   using MediaPortal.GUI.Library;
@@ -43,9 +45,10 @@ namespace MyFilmsPlugin.MyFilms.Utils
   using System.Security.Cryptography;
 
   using TraktPlugin;
-  using TraktPlugin.TraktAPI;
   using TraktPlugin.TraktAPI.DataStructures;
   using System.Xml;
+
+  using ConnectionState = TraktPlugin.TraktAPI.ConnectionState;
 
   #region String Extension Methods
     public static class StringExtensions
@@ -929,6 +932,129 @@ namespace MyFilmsPlugin.MyFilms.Utils
           return size;
         }
     }
+
+    // usage: RetryUtility.RetryAction( () => SomeFunctionThatCanFail(), 3, 1000 );
+    public static class RetryUtility
+    {
+      public static void RetryAction(System.Action action, int numRetries, int retryTimeout)
+      {
+        if (action == null)
+          throw new ArgumentNullException("action"); // slightly safer...
+
+        do
+        {
+          try { action(); return; }
+          catch
+          {
+            if (numRetries <= 0) throw;  // improved to avoid silent failure
+            else Thread.Sleep(retryTimeout);
+          }
+        } while (numRetries-- > 0);
+      }
+    }
+
+    /// <summary>
+    /// Summary description for SQLOps.
+    /// </summary>
+    public class SQLOps
+    {
+      public SQLOps()
+      {
+      }
+
+      //FJC = First Join Column
+      //SJC = Second Join Column
+
+      public static DataTable Join(DataTable First, DataTable Second, DataColumn[] FJC, DataColumn[] SJC)
+      {
+        //Create Empty Table
+        DataTable table = new DataTable("Join");
+
+        // Use a DataSet to leverage DataRelation
+        using (DataSet ds = new DataSet())
+        {
+          //Add Copy of Tables
+          ds.Tables.AddRange(new DataTable[] { First.Copy(), Second.Copy() });
+
+          //Identify Joining Columns from First
+          DataColumn[] parentcolumns = new DataColumn[FJC.Length];
+
+          for (int i = 0; i < parentcolumns.Length; i++)
+          {
+            parentcolumns[i] = ds.Tables[0].Columns[FJC[i].ColumnName];
+          }
+
+          //Identify Joining Columns from Second
+          DataColumn[] childcolumns = new DataColumn[SJC.Length];
+
+          for (int i = 0; i < childcolumns.Length; i++)
+          {
+            childcolumns[i] = ds.Tables[1].Columns[SJC[i].ColumnName];
+          }
+
+          //Create DataRelation
+          DataRelation r = new DataRelation(string.Empty, parentcolumns, childcolumns, false);
+          ds.Relations.Add(r);
+
+          //Create Columns for JOIN table
+          for (int i = 0; i < First.Columns.Count; i++)
+          {
+            table.Columns.Add(First.Columns[i].ColumnName, First.Columns[i].DataType);
+          }
+
+          for (int i = 0; i < Second.Columns.Count; i++)
+          {
+            //Beware Duplicates
+            if (!table.Columns.Contains(Second.Columns[i].ColumnName)) table.Columns.Add(Second.Columns[i].ColumnName, Second.Columns[i].DataType);
+            else table.Columns.Add(Second.Columns[i].ColumnName + "_Second", Second.Columns[i].DataType);
+          }
+
+
+          //Loop through First table
+          table.BeginLoadData();
+
+          foreach (DataRow firstrow in ds.Tables[0].Rows)
+          {
+            //Get "joined" rows
+            DataRow[] childrows = firstrow.GetChildRows(r);
+            if (childrows != null && childrows.Length > 0)
+            {
+              object[] parentarray = firstrow.ItemArray;
+              foreach (DataRow secondrow in childrows)
+              {
+                object[] secondarray = secondrow.ItemArray;
+                object[] joinarray = new object[parentarray.Length + secondarray.Length];
+                Array.Copy(parentarray, 0, joinarray, 0, parentarray.Length);
+                Array.Copy(secondarray, 0, joinarray, parentarray.Length, secondarray.Length);
+                table.LoadDataRow(joinarray, true);
+              }
+            }
+            else
+            {
+              object[] parentarray = firstrow.ItemArray;
+              object[] joinarray = new object[parentarray.Length];
+              Array.Copy(parentarray, 0, joinarray, 0, parentarray.Length);
+              table.LoadDataRow(joinarray, true);
+            }
+          }
+          table.EndLoadData();
+        }
+
+        return table;
+      }
+
+      public static DataTable Join(DataTable First, DataTable Second, DataColumn FJC, DataColumn SJC)
+      {
+        return SQLOps.Join(First, Second, new DataColumn[] { FJC }, new DataColumn[] { SJC });
+      }
+
+      public static DataTable Join(DataTable First, DataTable Second, string FJC, string SJC)
+      {
+        return SQLOps.Join(
+          First, Second, new DataColumn[] { First.Columns[FJC] }, new DataColumn[] { First.Columns[SJC] });
+      }
+    }
+
 
 }
 
