7d2b5b6f2f3a14a65a7b75ea9904e7023be84009
Mikeplanet5 <Mikeplanet5@web.de>
2013-06-22 09:34:37 +0000

    (WIP issue #329) : Current MyFilms is incompatible with MP 1.4.0 Add MP1.4 detection to installer; change API version

--- MyFilms/Properties/AssemblyInfo.cs
+++ MyFilms/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using MediaPortal.Common.Utils;
 //[assembly: AssemblyFileVersion("5.0.2.1372")]
 //[assembly: NeutralResourcesLanguageAttribute("")]
 
-[assembly: CompatibleVersion("1.2.100.0", "1.1.6.27644")]
+[assembly: CompatibleVersion("1.3.100.0", "1.1.6.27644")]
 [assembly: UsesSubsystem("MP.SkinEngine")]
 [assembly: UsesSubsystem("MP.Config")]
 [assembly: UsesSubsystem("MP.Players.Video")]
