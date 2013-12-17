d6328d8b6beb304cdc5d3af1a9b3d82d71625cae
GuzziMP <GuzziMP@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2013-12-17 00:26:03 +0000

    (wip issue #352) : plugin fanart loading not working anymore - TMDB API v2.1 was retired on sep. 15th modify tmdb wrapper methods to use v3 api

--- Grabber/TMDBv3/TmdbMovie.cs
+++ Grabber/TMDBv3/TmdbMovie.cs
@@ -5,6 +5,37 @@ using System.Text;
 
 namespace WatTmdb.V3
 {
+    public class TmdbMovie
+    {
+      public int id { get; set; }
+      public bool adult { get; set; }
+      public string backdrop_path { get; set; }
+      public BelongsToCollection belongs_to_collection { get; set; }
+      public int budget { get; set; }
+      public List<MovieGenre> genres { get; set; }
+      public string homepage { get; set; }
+      public string imdb_id { get; set; }
+      public string original_title { get; set; }
+      public string overview { get; set; }
+      public double popularity { get; set; }
+      public string poster_path { get; set; }
+      public List<ProductionCompany> production_companies { get; set; }
+      public List<ProductionCountry> production_countries { get; set; }
+      public string release_date { get; set; }
+      public Int64 revenue { get; set; }
+      public int runtime { get; set; }
+      public List<SpokenLanguage> spoken_languages { get; set; }
+      public string tagline { get; set; }
+      public string title { get; set; }
+      public double vote_average { get; set; }
+      public int vote_count { get; set; }
+
+      public override string ToString()
+      {
+        return title;
+      }
+    }
+  
     public class BelongsToCollection
     {
         public int id { get; set; }
@@ -57,34 +88,4 @@ namespace WatTmdb.V3
         }
     }
 
-    public class TmdbMovie
-    {
-        public bool adult { get; set; }
-        public string backdrop_path { get; set; }
-        public BelongsToCollection belongs_to_collection { get; set; }
-        public int budget { get; set; }
-        public List<MovieGenre> genres { get; set; }
-        public string homepage { get; set; }
-        public int id { get; set; }
-        public string imdb_id { get; set; }
-        public string original_title { get; set; }
-        public string overview { get; set; }
-        public double popularity { get; set; }
-        public string poster_path { get; set; }
-        public List<ProductionCompany> production_companies { get; set; }
-        public List<ProductionCountry> production_countries { get; set; }
-        public string release_date { get; set; }
-        public Int64 revenue { get; set; }
-        public int runtime { get; set; }
-        public List<SpokenLanguage> spoken_languages { get; set; }
-        public string tagline { get; set; }
-        public string title { get; set; }
-        public double vote_average { get; set; }
-        public int vote_count { get; set; }
-
-        public override string ToString()
-        {
-            return title;
-        }
-    }
 }
