396aa6eb0ac79f10563b60a40ef34cfcccf287b5
GuzziMP <GuzziMP@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2014-02-07 11:31:56 +0000

    (WIP issue #372) : MP 1.6 compatibility - move files and cleanup

--- Grabber_Interface/Merge/ilmerge.bat
+++ Grabber_Interface/Merge/ilmerge.bat
@@ -1,4 +1,4 @@
 @echo off
 IF EXIST MyFilms_Grabber_Interface_UNMERGED.exe del MyFilms_Grabber_Interface_UNMERGED.exe
 ren MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe
-ilmerge /out:MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe NLog.dll Grabber.dll
+ilmerge /out:MyFilms_Grabber_Interface.exe MyFilms_Grabber_Interface_UNMERGED.exe NLog.dll Grabber.dll  /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
--- MyFilms/Merge/ilmerge.bat
+++ MyFilms/Merge/ilmerge.bat
@@ -1,5 +1,5 @@
 @echo off
 IF EXIST MyFilms_UNMERGED.dll del MyFilms_UNMERGED.dll
 ren MyFilms.dll MyFilms_UNMERGED.dll
-ilmerge /out:MyFilms.dll MyFilms_UNMERGED.dll NLog.dll Grabber.dll TaskScheduler.dll
+ilmerge /out:MyFilms.dll MyFilms_UNMERGED.dll NLog.dll Grabber.dll TaskScheduler.dll /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
 rem ilmerge /out:MesFilms.dll MesFilms_UNMERGED.dll Cornerstone.MP.dll NLog.dll AMCupdater.exe TaskScheduler.dll
