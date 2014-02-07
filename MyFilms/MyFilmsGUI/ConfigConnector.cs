213bc5db6130ab6da5d19d8847872092cb4778a7
GuzziMP <GuzziMP@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2014-02-07 11:20:27 +0000

    (WIP issue #372) : MP 1.6 compatibility - fix installer and pluginconfigloader for .Net conflicts

---
+++ MyFilms/MyFilmsGUI/ConfigConnector.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaPortal.GUI.Library;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Threading;
+using MediaPortal.Configuration;
+
+namespace MyFilmsPlugin.MyFilms.Configuration
+{
+  [PluginIcons("MyFilmsPlugin.Resources.film-reel-128x128.png", "MyFilmsPlugin.Resources.film-reel-128x128-faded.png")]
+  public class ConfigConnector : ISetupForm
+  {
+    #region ISetupForm Members
+
+    // Returns the name of the plugin which is shown in the plugin menu
+    public string PluginName()
+    {
+      return "MyFilms";
+    }
+
+    // Returns the description of the plugin is shown in the plugin menu
+    public string Description()
+    {
+      return "MyFilms Ant Movie Catalog - Guzzi Mod";
+    }
+
+    // Returns the author of the plugin which is shown in the plugin menu
+    public string Author()
+    {
+      return "Zebons (Mod by Guzzi)";
+    }
+
+    // show the setup dialog
+    public void ShowPlugin()
+    {
+      MyFilmsSetup setup = new MyFilmsSetup();
+      setup.ShowDialog();
+    }
+
+    // Indicates whether plugin can be enabled/disabled
+    public bool CanEnable()
+    {
+      return true;
+    }
+
+    // get ID of windowplugin belonging to this setup
+    public int GetWindowId()
+    {
+      return 7986;
+    }
+
+    // Indicates if plugin is enabled by default;
+    public bool DefaultEnabled()
+    {
+      return true;
+    }
+
+    // indicates if a plugin has its own setup screen
+    public bool HasSetup()
+    {
+      return true;
+    }
+
+
+    /// <summary>
+    /// If the plugin should have its own button on the main menu of Media Portal then it
+    /// should return true to this method, otherwise if it should not be on home
+    /// it should return false
+    /// </summary>
+    /// <param name="strButtonText">text the button should have</param>
+    /// <param name="strButtonImage">image for the button, or empty for default</param>
+    /// <param name="strButtonImageFocus">image for the button, or empty for default</param>
+    /// <param name="strPictureImage">subpicture for the button or empty for none</param>
+    /// <returns>true  : plugin needs its own button on home
+    ///          false : plugin does not need its own button on home</returns>
+    public bool GetHome(out string strButtonText, out string strButtonImage, out string strButtonImageFocus, out string strPictureImage)
+    {
+      strButtonText = PluginNameSetup();
+      strButtonImage = String.Empty;
+      strButtonImageFocus = String.Empty;
+      strPictureImage = String.Format("hover_{0}.png", "MyFilms");
+      string strBtnFile = String.Format(@"{0}\media\{1}", GUIGraphicsContext.Skin, strPictureImage);
+      if (!System.IO.File.Exists(strBtnFile))
+        strPictureImage = string.Empty;
+      return true;
+    }
+    #endregion
+
+    private string strPluginName = null;
+    private string PluginNameSetup()
+    {
+      if (strPluginName == null)
+      {
+        string wPluginName;
+        using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "MyFilms.xml")))
+        {
+          wPluginName = xmlreader.GetValueAsString("MyFilms", "PluginName", "MyFilms");
+        }
+        strPluginName = wPluginName;
+      }
+      return strPluginName;
+    }
+  }
+}
---
+++ PluginConfigLoader/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using MediaPortal.GUI.Library;
+
+namespace PluginConfigLoader {
+    static class Program {
+
+        private static string pluginFile = string.Empty;
+        private static string[] assemblies = null;
+
+        enum MsgBoxIcon {
+            INFO,
+            WARNING,
+            CRITICAL
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] arguments) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            // Exit if we could not process the command line arguments
+            if (!ProcessArguments(arguments)) {
+                return;
+            }
+
+            // Load Plugin Configuration
+            LoadPluginConfiguration();
+
+        }
+
+        private static bool ProcessArguments(string[] arguments) {            
+            // Check if there are any arguments
+            if (arguments == null || arguments.Length == 0) {
+                string message = "No Arguments were specified on the command line.\n\n";
+                message += "Usage: PluginConfigLoader /plugin=<filename>\n\n";
+                message += "Optional Parameter: /loadassembly=<filename1>;<filename2>...";
+                ShowMessage(message, MsgBoxIcon.INFO);
+                return false;
+            }
+
+            // App only supports two argument
+            string trimmedArgument1 = arguments[0].ToLower();
+            string trimmedArgument2 = string.Empty;
+            // 2nd argument is optional and can include a list of assemblies to load
+            // each one seperated by a semi-colon
+            if (arguments.Length == 2)            
+                trimmedArgument2 = arguments[1].ToLower();
+
+            // Valid argument starts with /plugin
+            if (!trimmedArgument1.StartsWith("/plugin")) {
+                string message = "Invalid argument specified on the command line.\n\n";
+                message += "Usage: PluginConfigLoader /plugin=<filename>";                
+                ShowMessage(message, MsgBoxIcon.INFO);
+                return false;
+            }
+            
+            // Get Filename of plugin
+            string[] subArguments = trimmedArgument1.Split('=');
+
+            if (subArguments.Length < 2) {
+                string message = "No filename specified in command line argument.\n\n";
+                message += "Usage: PluginConfigLoader /plugin=<filename>";
+                ShowMessage(message, MsgBoxIcon.INFO);
+                return false;
+            }
+            pluginFile = subArguments[1];
+            
+            // Get Optional arguments
+            // Valid argument starts with /loadassembly
+            if (trimmedArgument2.StartsWith("/loadassembly")) {
+                // Get list of assemblies to load with plugin
+                string[] assemblySplits = trimmedArgument2.Split('=');
+                if (assemblySplits.Length == 2)                
+                    assemblies = assemblySplits[1].ToString().Split(';');
+            }
+            
+            return true;
+        }
+
+        public static void LoadPluginConfiguration() {
+            if (!File.Exists(pluginFile)) {
+                string message = string.Format("Plugin not found: {0}", pluginFile);
+                ShowMessage(message, MsgBoxIcon.INFO);
+                return;
+            }
+
+            // Flag if plugin has a configuration form
+            bool hasConfigForm = false;
+
+            try {
+                // Get Plugin Assembly and check if its valid
+                Assembly pluginAssembly = Assembly.LoadFrom(pluginFile);
+
+                if (pluginAssembly == null) {
+                    string message = string.Format("Invalid Plugin: {0}", pluginFile);
+                    ShowMessage(message, MsgBoxIcon.INFO);
+                }
+
+                Type[] exportedTypes = pluginAssembly.GetExportedTypes();
+
+                foreach (Type type in exportedTypes) {
+                    if (type.IsAbstract) {
+                        continue;
+                    }
+
+                    if (type.GetInterface("MediaPortal.GUI.Library.ISetupForm") != null) {
+                        // Load any optional assemblies to support the plugin
+                        if (assemblies != null) {
+                            foreach (string assembly in assemblies) {
+                                if (File.Exists(assembly)) {
+                                    try {
+                                        Assembly.LoadFile(assembly);
+                                    }
+                                    catch (Exception e){
+                                        string message = string.Format("Exception in plugin SetupForm loading: \n\n{0} ", e.Message);
+                                        ShowMessage(message, MsgBoxIcon.CRITICAL);                                        
+                                    }
+                                }
+                            }
+                        }
+
+                        try {                           
+                            // Create instance of the current type                           
+                            object pluginObject = Activator.CreateInstance(type);
+                            ISetupForm pluginForm = pluginObject as ISetupForm;                            
+
+                            // If the plugin has a Configuration form, show it
+                            if (pluginForm != null) {
+                                if (pluginForm.HasSetup()) {
+                                    hasConfigForm = true;
+                                    pluginForm.ShowPlugin();
+                                    return;
+                                }                                   
+                            }
+                        }
+                        catch (Exception setupFormException) {
+                            string message = string.Format("Exception in plugin SetupForm loading: \n\n{0} ", setupFormException.Message);
+                            ShowMessage(message, MsgBoxIcon.CRITICAL);
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception unknownException) {
+                string message = string.Format("Exception in plugin loading: \n\n{0}", unknownException.Message);
+                ShowMessage(message, MsgBoxIcon.CRITICAL);
+            }
+
+            if (!hasConfigForm) {
+                string message = "Plugin does not have a configuration form to show";
+                ShowMessage(message, MsgBoxIcon.INFO);
+            }
+
+        }
+
+        private static void ShowMessage(string message, MsgBoxIcon icon) {
+            MessageBoxIcon MessageBoxIcon = MessageBoxIcon.Information;
+            if (icon == MsgBoxIcon.CRITICAL) MessageBoxIcon = MessageBoxIcon.Error;
+            MessageBox.Show(message, "Plugin Config Loader", MessageBoxButtons.OK, MessageBoxIcon);
+            return;
+        }
+
+    }
+}
