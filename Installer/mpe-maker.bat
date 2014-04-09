f998ec2356c0391dd6cd3e70a204b4fc41b2bb2b
GuzziMP <GuzziMP@b6ee4827-db72-31d7-dadd-f97e4095b4f5>
2014-04-09 04:03:30 +0000

    (Resolves issue #372) : MP 1.6 compatibility - Fix installer message about MP version - Set MF version to 6.2.0

--- Installer/mpe-maker.bat
+++ Installer/mpe-maker.bat
@@ -1,1 +1,3 @@
-"C:\Programme\Team MediaPortal\MediaPortal\MpeMaker.exe" MyFilms-12.xmp2 /V=6.0.3.2893 /B
+set progpath=%ProgramFiles%
+if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
+"%progpath%\Team MediaPortal\MediaPortal\MpeMaker.exe" MyFilms-12.xmp2 /V=6.0.3.2979 /B
